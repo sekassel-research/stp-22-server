437d12c0d382f201e04696ec49b530e31597fea9
Adrian Kunz <clashsoft@hotmail.com>
2021-05-27 14:30:26 +0200

    feat: Add message module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageModule } from './message/message.module';
 
 const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/stpss21';
 
 @Module({
-  imports: [MongooseModule.forRoot(mongoUri)],
+  imports: [
+    MongooseModule.forRoot(mongoUri),
+    MessageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
