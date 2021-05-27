f9d7ccb9d190d29521fd43445d16867e5843c05d
Adrian Kunz <clashsoft@hotmail.com>
2021-05-27 14:24:51 +0200

    feat: Set up Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/stpss21';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoUri)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
