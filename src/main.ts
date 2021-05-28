b8a5934c4b08ff7133537f1ea961b551236bb9a7
Adrian Kunz <clashsoft@hotmail.com>
2021-05-28 16:05:43 +0200

    feat: Add Swagger OpenAPI available at GET /api

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,16 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useWebSocketAdapter(new WsAdapter(app));
+
+  const config = new DocumentBuilder()
+    .setTitle('STP Server')
+    .setDescription('The STP server for summer term 2022')
+    .setVersion('1.0')
+    .addTag('stp')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
