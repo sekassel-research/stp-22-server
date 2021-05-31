9da493fdc718509302440b5f32ad6fb4d26e38cd
Adrian Kunz <clashsoft@hotmail.com>
2021-06-01 00:46:55 +0200

    feat: Trigger events on member create, update and delete

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../user/user.dto';
@@ -9,11 +10,14 @@ import { Member } from './member.schema';
 export class MemberService {
   constructor(
     @InjectModel('members') private model: Model<Member>,
+    private eventEmitter: EventEmitter2,
   ) {
   }
 
   async create(gameId: string, user: User, member: CreateMemberDto): Promise<Member> {
-    return this.model.create({ ...member, userId: user.id, gameId });
+    const created = await this.model.create({ ...member, userId: user.id, gameId });
+    created && this.eventEmitter.emit('member.created', created);
+    return created;
   }
 
   async findAll(gameId: string): Promise<Member[]> {
@@ -25,10 +29,14 @@ export class MemberService {
   }
 
   async update(gameId: string, userId: string, member: UpdateMemberDto): Promise<Member | undefined> {
-    return this.model.findOneAndUpdate({ gameId, userId }, member).exec();
+    const updated = await this.model.findOneAndUpdate({ gameId, userId }, member).exec();
+    updated && this.eventEmitter.emit('member.updated', updated);
+    return updated;
   }
 
   async delete(gameId: string, userId: string): Promise<Member | undefined> {
-    return this.model.findOneAndDelete({ gameId, userId }).exec();
+    const deleted = await this.model.findOneAndDelete({ gameId, userId }).exec();
+    deleted && this.eventEmitter.emit('member.deleted', deleted);
+    return deleted;
   }
 }
