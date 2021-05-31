d77fb7e9297433d683e79583e2d78c6b9be5d4df
Adrian Kunz <clashsoft@hotmail.com>
2021-06-01 01:19:52 +0200

    refactor: Remove unused event classes

--- src/message/message.event.ts
+++
@@ -1,7 +0,0 @@
-import { WsResponse } from '@nestjs/websockets';
-import { MessageDocument } from './message.schema';
-
-export interface MessageEvent extends WsResponse<MessageDocument> {
-  event: 'created' | 'updated' | 'deleted';
-  data: MessageDocument;
-}
--- src/user/user.event.ts
+++
@@ -1,6 +0,0 @@
-import { WsResponse } from '@nestjs/websockets';
-import { User } from './user.dto';
-
-export interface UserEvent extends WsResponse<User> {
-  event: 'online' | 'offline';
-}
