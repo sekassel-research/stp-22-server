a569f65033a929998a3173d32b7f200c5faf0e3b
Adrian Kunz <clashsoft@hotmail.com>
2021-06-02 18:48:43 +0200

    feat: User-visible events
    By passing a second data argument to eventEmitter.emit(), a producer can
    now specify which users should be able to receive the event.

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -15,9 +15,9 @@ export class AppGateway implements OnGatewayConnection {
 
   private unsubscribeRequests = new Subject<{ client: any, event: string }>();
 
-  async handleConnection(client: WebSocket, message: IncomingMessage): Promise<void> {
+  async handleConnection(client: any, message: IncomingMessage): Promise<void> {
     try {
-      await this.authService.parseUserForWebSocket(message);
+      client.user = await this.authService.parseUserForWebSocket(message);
     } catch (err) {
       client.send(JSON.stringify(err));
       client.close();
@@ -26,14 +26,17 @@ export class AppGateway implements OnGatewayConnection {
 
   @SubscribeMessage('subscribe')
   subscribe(client: any, event: string): Observable<WsResponse<unknown>> {
-    return this.observe(event).pipe(
+    return this.observe(client, event).pipe(
       takeUntil(this.unsubscribeRequests.pipe(filter(unsub => unsub.client === client && unsub.event === event))),
     );
   }
 
-  private observe<T>(event: string): Observable<WsResponse<T>> {
+  private observe<T>(client: any, event: string): Observable<WsResponse<T>> {
     return new Observable<WsResponse<T>>(observer => {
-      const handler = function(data: T) {
+      const handler = function(data: T, users?: string[]) {
+        if (users && !users.includes(client.user.id)) {
+          return;
+        }
         observer.next({
           event: this.event,
           data,
