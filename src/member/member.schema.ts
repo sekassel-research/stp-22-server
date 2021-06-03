10414c816e98cd3f3bf54170d2d2ed40a1126726
Adrian Kunz <clashsoft@hotmail.com>
2021-06-03 18:27:12 +0200

    fix: Properly omit _id in Member JSON

--- src/member/member.schema.ts
+++ src/member/member.schema.ts
@@ -4,10 +4,7 @@ import { IsMongoId, IsUUID } from 'class-validator';
 import { Document } from 'mongoose';
 import { GLOBAL_SCHEMA_OPTIONS, GlobalSchema } from '../util/schema';
 
-@Schema({
-  ...GLOBAL_SCHEMA_OPTIONS,
-  id: false,
-})
+@Schema(GLOBAL_SCHEMA_OPTIONS)
 export class Member extends OmitType(GlobalSchema, ['_id' as const]) {
   @Prop()
   @ApiProperty({ format: 'objectid', example: '507f191e810c19729de860ea' })
@@ -24,4 +21,11 @@ export type MemberDocument = Member & Document;
 
 export const MemberSchema = SchemaFactory.createForClass(Member)
   .index({ gameId: 1, userId: 1 }, { unique: true })
+  .set('toJSON', {
+    virtuals: true,
+    transform: (doc, converted) => {
+      delete converted._id;
+      delete converted.id;
+    },
+  })
 ;
