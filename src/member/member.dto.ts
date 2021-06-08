7cb72f3965132256eff66654f70255112d9b29f2
Adrian Kunz <clashsoft@hotmail.com>
2021-06-09 00:03:43 +0200

    fix: Improve type declaration for UpdateMemberDto

--- src/member/member.dto.ts
+++ src/member/member.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
+import { ApiProperty, PartialType, PickType } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { Member } from './member.schema';
 
@@ -9,5 +9,5 @@ export class CreateMemberDto extends PickType(Member, [] as const) {
   password: string;
 }
 
-export class UpdateMemberDto extends PickType(CreateMemberDto, [] as const) {
+export class UpdateMemberDto extends PartialType(PickType(Member, [] as const)) {
 }
