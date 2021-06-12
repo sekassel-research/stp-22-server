b82d3c25c835c7eba9b5f3fe771e7ac9b86511f7
Adrian Kunz <clashsoft@hotmail.com>
2021-06-13 01:11:26 +0200

    refactor: Add random.ts helper

--- src/settlers/map/map.service.ts
+++ src/settlers/map/map.service.ts
@@ -4,19 +4,8 @@ import { Model } from 'mongoose';
 import { Game } from '../../game/game.schema';
 import { RESOURCE_TILE_TYPES, TileType, WEIGHTED_NUMBER_TOKENS } from '../shared/constants';
 import { cubeCircle } from '../shared/hexagon';
+import { randInt, shuffle } from '../shared/random';
 import { Map, Tile } from './map.schema';
-
-function randInt(maxExclusive: number): number {
-  return Math.floor(Math.random() * maxExclusive);
-}
-
-function shuffle(a) { // fisher-yates shuffle
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = randInt(i + 1);
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
 
 @Injectable()
 export class MapService {
