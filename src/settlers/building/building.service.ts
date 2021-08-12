ecbbb56217e02cbe670922a928d21f2ba9466359
Adrian Kunz <clashsoft@hotmail.com>
2021-08-12 14:16:44 +0200

    fix: Add Building created event

--- src/settlers/building/building.service.ts
+++ src/settlers/building/building.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Model } from 'mongoose';
+import { Document, FilterQuery, Model } from 'mongoose';
+import { EventService } from '../../event/event.service';
 import { Building } from './building.schema';
 
 @Injectable()
 export class BuildingService {
   constructor(
     @InjectModel('buildings') private model: Model<Building>,
+    private eventEmitter: EventService,
   ) {
   }
 
@@ -15,10 +17,16 @@ export class BuildingService {
   }
 
   async create(building: Building): Promise<Building> {
-    return this.model.create(building);
+    const created = await this.model.create(building);
+    created && this.emit('created', created);
+    return created;
   }
 
   async deleteByGame(gameId: string): Promise<void> {
     await this.model.deleteMany({ gameId }).exec();
+  }
+
+  private emit(event: string, building: Building & Document) {
+    this.eventEmitter.emit(`games.${building.gameId}.buildings.${building._id}.${event}`, building); // TODO visibility
   }
 }
