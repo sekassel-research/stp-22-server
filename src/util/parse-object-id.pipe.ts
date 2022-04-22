27e723ca1749e12ee8d26c3600e9a49b6277ce38
Adrian Kunz <clashsoft@hotmail.com>
2022-04-22 16:32:06 +0200

    fix: Remove ObjectId conversion

--- src/util/parse-object-id.pipe.ts
+++ src/util/parse-object-id.pipe.ts
@@ -1,14 +1,10 @@
 import { ArgumentMetadata, BadRequestException, PipeTransform } from '@nestjs/common';
-import { ObjectId } from 'bson';
 import { isMongoId } from 'class-validator';
 
 export class ParseObjectIdPipe implements PipeTransform<string> {
-  transform(value: string, metadata: ArgumentMetadata): string | ObjectId {
+  transform(value: string, metadata: ArgumentMetadata): string {
     if (!isMongoId(value)) {
       throw new BadRequestException(`Invalid Object ID '${value}' for parameter '${metadata.data}'`);
-    }
-    if (metadata.metatype === ObjectId) {
-      return new ObjectId(value);
     }
     return value;
   }
