a59479fa5f1bcd13795a85a59b0930efd4c761dd
Adrian Kunz <clashsoft@hotmail.com>
2022-05-07 14:33:01 +0200

    fix: Maximum group member array size

--- src/group/group.schema.ts
+++ src/group/group.schema.ts
@@ -1,8 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsMongoId, IsOptional, IsString, Length } from 'class-validator';
+import { ArrayMaxSize, IsMongoId, IsOptional, IsString, Length } from 'class-validator';
 import { Document } from 'mongoose';
 import { GLOBAL_SCHEMA_OPTIONS, GlobalSchema, MONGO_ID_ARRAY_FORMAT } from '../util/schema';
+
+const MAX_MEMBERS = 100;
 
 @Schema(GLOBAL_SCHEMA_OPTIONS)
 export class Group extends GlobalSchema {
@@ -15,7 +17,8 @@ export class Group extends GlobalSchema {
 
   @Prop()
   @IsMongoId({ each: true })
-  @ApiProperty(MONGO_ID_ARRAY_FORMAT)
+  @ApiProperty({ ...MONGO_ID_ARRAY_FORMAT, maxItems: MAX_MEMBERS })
+  @ArrayMaxSize(MAX_MEMBERS)
   members: string[];
 }
 
