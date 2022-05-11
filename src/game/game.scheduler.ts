df0b2ac354e2698ab29ecd26cb10ef7968925fb1
Adrian Kunz <clashsoft@hotmail.com>
2022-05-11 17:54:40 +0200

    fix: Log game deletion

--- src/game/game.scheduler.ts
+++ src/game/game.scheduler.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { environment } from '../environment';
 import { GameService } from './game.service';
 
 @Injectable()
 export class GameScheduler {
+  private logger = new Logger('Game Cleaner');
+
   constructor(
     private gameService: GameService,
   ) {
@@ -13,6 +15,9 @@ export class GameScheduler {
   @Cron(CronExpression.EVERY_10_MINUTES)
   async deleteEmptyGames(): Promise<void> {
     const maxAgeMs = environment.cleanup.deleteGameAfterHours * 60 * 60 * 1000;
-    await this.gameService.deleteOldGames(maxAgeMs);
+    const games = await this.gameService.deleteOldGames(maxAgeMs);
+    if (games.length) {
+      this.logger.warn(`Deleted ${games.length} old games.`);
+    }
   }
 }
