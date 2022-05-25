e0ad1d8cfdacf63ca4b28aa2c23ced9310efdade
Adrian Kunz <clashsoft@hotmail.com>
2022-05-25 09:15:35 +0200

    docs: Document CreateMoveDto building

--- src/settlers/move/move.dto.ts
+++ src/settlers/move/move.dto.ts
@@ -7,7 +7,13 @@ export class CreateMoveDto extends PickType(Move, [
   'action',
 ] as const) {
   @Prop()
-  @ApiProperty({ type: CreateBuildingDto, required: false })
+  @ApiProperty({
+    type: CreateBuildingDto,
+    required: false,
+    description: 'Ignored if action is not `build`. ' +
+      'If set, the building will be placed and the player will stay in turn. ' +
+      'If unset, the current player\'s turn ends.',
+  })
   building?: CreateBuildingDto;
 }
 
