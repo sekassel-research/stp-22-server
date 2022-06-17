418645aead5d336b9b2693595d659df9050b7110
Adrian Kunz <clashsoft@hotmail.com>
2022-06-17 11:29:50 +0200

    fix: Automatically steal resources from inactive players on 7

--- src/settlers/move/game-logic/roll.service.ts
+++ src/settlers/move/game-logic/roll.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/com
 import { BuildingService } from '../../building/building.service';
 import { Tile } from '../../map/map.schema';
 import { MapService } from '../../map/map.service';
+import { Player } from '../../player/player.schema';
 import { PlayerService } from '../../player/player.service';
 import { ResourceType, TILE_RESOURCES } from '../../shared/constants';
 import { cubeCorners } from '../../shared/hexagon';
@@ -42,7 +43,9 @@ export class RollService {
 
   async roll(gameId: string, userId: string, move: CreateMoveDto): Promise<Move> {
     const roll = this.d6() + this.d6();
-    if (roll !== 7) {
+    if (roll === 7) {
+      await this.roll7(gameId);
+    } else {
       await this.rollResources(gameId, roll);
     }
 
@@ -54,6 +57,46 @@ export class RollService {
       roll,
     });
   }
+
+  private async roll7(gameId: string): Promise<void> {
+    const players = await this.playerService.findAll(gameId, {active: false});
+    await Promise.all(players.map(p => this.stealResources(p)));
+  }
+
+  private async stealResources(player: Player) {
+    const resources = { ...player.resources };
+    let total = Object.values(resources).sum();
+    if (total <= 7) {
+      return;
+    }
+
+    const keys = Object.keys(resources) as ResourceType[];
+    const stealCount = Math.floor(total / 2);
+
+    for (let i = 0; i < stealCount; i++) {
+      let rand = Math.randInt(total);
+      for (const key of keys) {
+        const amount = resources[key];
+        if (!amount) {
+          continue;
+        }
+
+        if (rand >= amount) {
+          rand -= amount;
+          continue;
+        }
+
+        resources[key] = amount - 1;
+        total--;
+        break;
+      }
+    }
+
+    await this.playerService.update(player.gameId, player.userId, {
+      resources,
+    });
+  }
+
 
   async rob(gameId: string, userId: string, move: CreateMoveDto): Promise<Move> {
     if (!move.rob) {
