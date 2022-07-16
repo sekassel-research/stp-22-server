41e72b5f166af9843ab2415c966c898957b2c739
Adrian Kunz <clashsoft@hotmail.com>
2022-07-16 16:23:27 +0200

    fix: Simplify longest road
    It is not necessary to reify the path, only the length is interesting

--- src/settlers/move/game-logic/longest-road.service.ts
+++ src/settlers/move/game-logic/longest-road.service.ts
@@ -4,17 +4,17 @@ import { edgeAdjacentEdges, Point3DWithEdgeSide } from '../../shared/hexagon';
 @Injectable()
 export class LongestRoadService {
   findLongestRoad(allRoads: Point3DWithEdgeSide[], start: Point3DWithEdgeSide): number {
-    let longestPath: Point3DWithEdgeSide[] = [];
-    for (const path of this.dfs(allRoads, start, new Set(), [start])) {
-      if (path.length >= longestPath.length) {
+    let longestPath = 0;
+    for (const path of this.dfs(allRoads, start, new Set(), 1)) {
+      if (path >= longestPath) {
         longestPath = path;
       }
     }
 
-    return longestPath.length;
+    return longestPath;
   }
 
-  private* dfs(roads: Point3DWithEdgeSide[], current: Point3DWithEdgeSide, seen: Set<Point3DWithEdgeSide>, path: Point3DWithEdgeSide[]): Generator<Point3DWithEdgeSide[]> {
+  private* dfs(roads: Point3DWithEdgeSide[], current: Point3DWithEdgeSide, seen: Set<Point3DWithEdgeSide>, path: number): Generator<number> {
     seen.add(current);
     for (const a of edgeAdjacentEdges(current)) {
       const road = roads.find(r => r.x === a.x && r.y === a.y && r.z === a.z && r.side === a.side);
@@ -22,7 +22,7 @@ export class LongestRoadService {
         continue;
       }
 
-      const newPath = [...path, road];
+      const newPath = path + 1;
       yield newPath;
       yield* this.dfs(roads, road, new Set(seen), newPath);
     }
